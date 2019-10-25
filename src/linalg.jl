336c8d9072e451a7a8d54e93fc27a31f7e6cd64e
Kai Xu <xukai921110@gmail.com>
2019-10-25 02:08:23 +0100

    improve type constraint

--- src/linalg.jl
+++ src/linalg.jl
@@ -49,9 +49,7 @@ end
 
 # Matrix division
 
-const CuOrAdj = Union{CuVecOrMat, LinearAlgebra.Adjoint{T, CuVecOrMat{T}}, LinearAlgebra.Transpose{T, CuVecOrMat{T}}} where {T<:AbstractFloat}
-
-function Base.:\(_A::AT1, _B::AT2) where {AT1<:CuOrAdj, AT2<:CuOrAdj}
+function Base.:\(_A::CuMatOrAdj, _B::CuOrAdj)
     A, B = copy(_A), copy(_B)
     A, ipiv = CuArrays.CUSOLVER.getrf!(A)
     return CuArrays.CUSOLVER.getrs!('N', A, ipiv, B)
